f689a7008b21fd52b99a055f22ad5a22e5a1079a
tatiana <tenb_@mail.ru>
2023-01-31 22:34:00 +0700

    Семинар 5

---
+++ Task38/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 38: 
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+// Массив вещественных чисел
+double[] CreateArrayRndDoudle(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.NextDouble() *(max - min) + min;
+    }
+    return array;
+}
+ 
+//  Массив вывода вещественных чисел
+void PrintArreyDouble (double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+       else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
