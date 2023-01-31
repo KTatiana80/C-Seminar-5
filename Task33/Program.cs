38dd718596d81e6d0f1d1729357c0ac5cae3fff3
tatiana <tenb_@mail.ru>
2023-01-31 12:45:40 +0700

    Семинар 5

---
+++ Task33/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 33: 
+// Задайте массив. 
+// Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да,
+
+//  Метод создания массива
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arrayRnd = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arrayRnd[i] = rnd.Next(min, max + 1);
+    }
+    return arrayRnd;
+}
+
+// Метод вывода массива
+void PrintArrey (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+       else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+// Метод поиска числа в массиве
+bool FindNumber(int[] array, int find)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == find)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Создание массива, ввод числа пользователем, вызов функции.
+Console.WriteLine("Укажите длину массива и число для поиска:");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumb = CreateArrayRndInt(sizeArray, -9, 9);
+PrintArrey(arrayNumb);
+
+Console.WriteLine("Укажите число для поиска:");
+int numberToFind = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FindNumber(arrayNumb, numberToFind) ? "Да" : "Нет");
---
+++ Task35/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 35: 
+// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в 
+// отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. 
+// В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+//  Метод создания массива
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+// Метод вывода массива
+void PrintArrey (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+       else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int ReturnNumberElements(int[] array, int min, int max)
+{
+    int numberElem = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max)
+        {
+            numberElem += 1;
+
+        }
+    }
+    return numberElem;
+}
+
+int[] arr = CreateArrayRndInt(123, 0, 100);
+PrintArrey(arr);
+
+int returnNumberElements = ReturnNumberElements(arr, 10, 99);
+Console.WriteLine($" -> {returnNumberElements}");
